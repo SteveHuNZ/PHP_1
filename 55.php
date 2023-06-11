5e699a72fa46a9b9d105084836ffa2e7891b9ee5
stevehu <stevehuinnz@gmail.com>
2023-06-11 21:47:21 +1200

    d

---
+++ 55.php
@@ -0,0 +1,12 @@
+<?php
+    $i = 0; // pre-set the loop control condition
+// do the loop control test BEFORE executing any loop statements
+
+    while ($i < 10) // perform the loop until $i is 10 or larger
+    {     
+        echo  "The number is: $i. ";  // this will print the numbers 0-9 in the console
+        $i++; //update the loop control counter;
+    }
+
+
+?>  
