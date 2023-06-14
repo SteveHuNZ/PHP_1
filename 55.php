c6a5e96c89bbf01d8e4d608247da2dac0fc72e97
stevehu <stevehuinnz@gmail.com>
2023-06-14 23:21:56 +1200

    dd

--- 55.php
+++ 55.php
@@ -1,12 +1,28 @@
 <?php
-    $i = 0; // pre-set the loop control condition
-// do the loop control test BEFORE executing any loop statements
+// Define your database credentials
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'your_username');
+define('DB_PASSWORD', 'your_password');
+define('DB_NAME', 'your_database');
 
-    while ($i < 10) // perform the loop until $i is 10 or larger
-    {     
-        echo  "The number is: $i. ";  // this will print the numbers 0-9 in the console
-        $i++; //update the loop control counter;
-    }
+// Define other constant values
+define('API_KEY', 'your_api_key');
+define('MAX_RESULTS', 10);
+define('MAX_RESULTS', 10);
 
+// Create an array to store all the configurations
+$config = array(
+    'db_host' => DB_HOST,
+    'db_username' => DB_USERNAME,
+    'db_password' => DB_PASSWORD,
+    'db_name' => DB_NAME,
+    'api_key' => API_KEY,
+    'max_results' => MAX_RESULTS
+);
 
-?>  
+// Serialize the configuration array and save it to a file
+$configFile = 'config.ini';
+file_put_contents($configFile, serialize($config));
+
+echo "Configuration file created successfully!";
+?>
